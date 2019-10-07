349302b3560e6af862a772410af1c4996d2f4f82
ds_070_8 <ds_070_8@anon.ece.uprm.edu>
2019-10-07 14:41:38 -0400

    lab commit

---
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/SortedCircularDoublyLinkedList.java
@@ -0,0 +1,98 @@
+package edu.uprm.cse.datastructures.cardealer;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+import edu.uprm.cse.datastructures.cardealer.util.SortedList;
+
+public class SortedCircularDoublyLinkedList<T> implements SortedList<Integer> {
+
+	public SortedCircularDoublyLinkedList(Comparator<Integer> cmp) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Integer obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean remove(Integer obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int removeAll(Integer obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Integer first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(Integer e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int firstIndex(Integer e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndex(Integer e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
