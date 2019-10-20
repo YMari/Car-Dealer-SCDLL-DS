7474df142252bf122ca906149a2fd5c07ecde7f7
yavier <yavier@YavierVM>
2019-10-20 17:30:58 -0400

    CarComparator fix

--- src/main/java/edu/uprm/cse/datastructures/cardealer/model/CarComparator.java
+++ src/main/java/edu/uprm/cse/datastructures/cardealer/model/CarComparator.java
@@ -6,38 +6,59 @@ public class CarComparator implements Comparator<Car> {
 
 	@Override
 	public int compare(Car car1, Car car2) {
-		String car1Str = car1.getCarBrand() + car1.getCarModel() + car1.getCarModelOption(); 
-		String car2Str = car2.getCarBrand() + car2.getCarModel() + car2.getCarModelOption();
+//		String car1Str = car1.getCarBrand() + car1.getCarModel() + car1.getCarModelOption(); 
+//		String car2Str = car2.getCarBrand() + car2.getCarModel() + car2.getCarModelOption();
+//		
+//	    if (car1Str == car2Str) 
+//	        return 0;
+//	    
+//	    if (car1Str == null) 
+//	    	return -1;
+//	    
+//	    if (car2Str == null) 
+//	        return 1;
+//	    
+//	    return car1Str.compareTo(car2Str);
+	    
+		String car1Brand = car1.getCarBrand(); 
+		String car2Brand = car2.getCarBrand();
 		
-	    if (car1Str == car2Str) 
-	        return 0;
-	    
-	    if (car1Str == null) 
-	    	return -1;
-	    
-	    if (car2Str == null) 
-	        return 1;
-	    
-	    return car1Str.compareTo(car2Str);
-	    
-//		String car1Str = car1.getCarBrand(); 
-//		String car2Str = car2.getCarBrand();
-//		int i = 0;
-//
-//		while (i < 2) {
-//			if (car1Str.compareTo(car2Str) > 0) { // car1 > car 2
-//				return 1;
-//			}
-//			else if (car1Str.compareTo(car2Str) < 0) { // car1 < car 2
-//				return -1;
-//			}
-//			else { // if the labeling was the same, swap to the next label and repeat
-//				car1Str = car1.getCarModel();
-//				car2Str = car2.getCarModel();
-//				i++;
-//			}
-//		}
-//		return 0; // Both cars are the same
+		String car1Model = car1.getCarModel(); 
+		String car2Model = car2.getCarModel();
+		
+		String car1Option = car1.getCarModelOption(); 
+		String car2Option = car2.getCarModelOption();
+		
+		if (car1Brand.compareTo(car2Brand) == 0) {
+			
+			if(car1Model.compareTo(car2Model) == 0) {
+				
+				if(car1Option.compareTo(car2Option) == 0) {
+					return 0;
+				}
+				else if (car1Option.compareTo(car2Option) > 0) {
+					return 1;
+				}
+				else {
+					return -1;
+				}
+				
+			}
+			else if (car1Model.compareTo(car2Model) > 0) {
+				return 1;
+			}
+			else {
+				return -1;
+			}
+			
+		}
+		else if (car1Brand.compareTo(car2Brand) > 0) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+		
 	}
 
 }
